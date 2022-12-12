129fc7cb832804a52fec6fb273a38b55cbf3f0bb
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2022-12-12 20:35:00 +0100

    Day 7

---
+++ Day 7 - No Space Left On Device/Day7.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day7 : IDayBase
+    {
+        List<Folder> SystemItems = new();
+        int fileSystemSize = 70000000;
+        int updateSpaceNeeded = 30000000;
+        public int Day => 7;
+
+        public void ExecutePart1()
+        {
+            Console.WriteLine(SystemItems.Count);
+            var atMost1000 = new List<SystemItem>();
+            foreach (var item in SystemItems.Where(x => x is Folder))
+            {
+                if (item.Size <= 100000)
+                    atMost1000.Add(item);
+            }
+            Console.WriteLine($"Result part 1: {atMost1000.Sum(x => x.Size)}");
+        }
+
+        public void ExecutePart2()
+        {
+            var unUsedSpace = fileSystemSize - SystemItems.First(x => x.Name == "/").Size;
+            
+            var stillNeeded = updateSpaceNeeded - unUsedSpace;
+            var biggerThenNeeded = SystemItems.Where(x => x.Size > stillNeeded).OrderBy(x=>x.Size);
+
+            //Wrong: 48381165
+            Console.WriteLine($"Result part 2: {biggerThenNeeded.First().Size}");
+        }
+
+        public void LoadInput()
+        {
+            string line;
+            var root = new Folder("/", null);
+            Folder currentDirectory = root;
+            SystemItems.Add(root);
+            //Add your own puzzle input to day7.txt
+            System.IO.StreamReader file = new System.IO.StreamReader($@"DayInputs\day{Day}.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "$ cd /")
+                    currentDirectory = root;
+                else if (line.StartsWith("dir"))
+                {
+                    var newFolder = new Folder(line, null);
+                    SystemItems.Add(newFolder);
+                    newFolder.SetParent(currentDirectory);
+                    newFolder.Parent.Children.Add(newFolder);
+                }
+                else if (line == "$ cd ..")
+                    currentDirectory = currentDirectory.Parent;
+                else if (line.StartsWith("$ cd"))
+                    currentDirectory = (Folder)currentDirectory.Children.First(x => x is Folder && ((Folder)x).Name.Equals($"dir {line.Split(' ')[2]}"));
+                else
+                    currentDirectory.Children.Add(new File(line, currentDirectory));
+            }
+        }
+    }
+}
---
+++ Day 7 - No Space Left On Device/SystemItem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    interface SystemItem
+    {
+        public string Name { get; }
+        public int Size { get; }
+        public Folder Parent { get; }
+        void SetParent(Folder parent);
+
+    }
+   
+    internal class File : SystemItem
+    {
+        string _name;
+        public string Name { get => _name; }
+        Folder _parent;
+        public Folder Parent { get => _parent; }
+
+        public int Size
+        {
+            get
+            {
+                if (Name.StartsWith("$ ls"))
+                    return 0;
+                return int.Parse(Name.Split(' ')[0]);
+            }
+
+        }
+
+        public void SetParent(Folder parent)
+        {
+            _parent = parent;
+        }
+
+        public File(string name, Folder parent)
+        {
+            _name = name;
+            _parent = parent;
+        }
+    }
+    internal class Folder : SystemItem
+    {
+        string _name;
+        public string Name { get => _name; }
+        Folder _parent;
+        public Folder Parent { get => _parent; }
+
+        public List<SystemItem> Children = new();
+        public int Size
+        {
+            get
+            {
+                var result = Children.Sum(x => x.Size);
+                return result;
+            }
+        }
+
+
+        public void SetParent(Folder parent)
+        {
+            _parent = parent;
+        }
+
+        public Folder(string name, Folder parent)
+        {
+            _name = name;
+            _parent = parent;
+        }
+    }
+}
